1dba311bde615fb7815dfd11c64da0feaa4264f8
hkmshb <hkmshb@gmail.com>
2015-10-07 12:50:57 +0100

    Added home page

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
 
 
 
 def home_page(request):
-    return HttpResponse('Cubicle !*')
+    return render(request, 'main/index.html')
