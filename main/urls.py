2d195bb3ca91e137fff1bba67f3067c0389e2bb5
hkmshb <hkmshb@gmail.com>
2015-10-07 14:57:35 +0100

    Added files to media folder for download

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
+import os
 from django.conf.urls import patterns, url
+from django.views.static import serve
+from django.conf import settings
 
 
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'home_page', name='home-page'),
+    url(r'^downloads/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(settings.MEDIA_ROOT, 'downloads'), }),
 )
